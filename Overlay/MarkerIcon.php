cd79644f5a12ba7e2f5c05c975c97f4a11dd8d8c
Menno Holtkamp <menno.holtkamp@shopforce.nl>
2016-09-19 16:04:23 +0200

    Improve PHPDoc and reformat

--- Overlay/MarkerIcon.php
+++ Overlay/MarkerIcon.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace PHPGoogleMaps\overlay;
+namespace PHPGoogleMaps\Overlay;
 
 /**
  * Marker Icon class
- * Attach these to markers to display custom marker icons
+ * Attach these to markers to display custom marker icons.
  *
  * Example:
  * $icon = new \PHPGoogleMaps\Marker\MarkerIcon( $icon_url );
@@ -13,186 +13,195 @@ namespace PHPGoogleMaps\overlay;
  * $marker->setIcon( $icon );
  * $marker->setShadow( $shadow );
  */
+class MarkerIcon  extends \PHPGoogleMaps\Core\MapObject
+{
+    /**
+     * Url of the icon image.
+     *
+     * @var string
+     */
+    protected $icon;
 
+    /**
+     * Width of the icon.
+     *
+     * @var int
+     */
+    protected $width;
 
-class MarkerIcon  extends \PHPGoogleMaps\Core\MapObject {
+    /**
+     * Height of the icon.
+     *
+     * @var int
+     */
+    protected $height;
 
-	/**
-	 * Url of the icon image
-	 *
-	 * @var string
-	 */
-	protected $icon;
+    /**
+     * X coordinate of the anchor.
+     *
+     * @var int
+     */
+    protected $anchor_x;
 
-	/**
-	 * Width of the icon
-	 *
-	 * @var integer
-	 */
-	protected $width;
+    /**
+     * Y coordinate of the anchor.
+     *
+     * @var int
+     */
+    protected $anchor_y;
 
-	/**
-	 * Height of the icon
-	 *
-	 * @var integer
-	 */
-	protected $height;
+    /**
+     * X coordinate of the origin.
+     *
+     * @var int
+     */
+    protected $origin_x;
 
-	/**
-	 * X coord of the anchor
-	 *
-	 * @var integer
-	 */
-	protected $anchor_x;
+    /**
+     * Y coordinate of the origin.
+     *
+     * @var int
+     */
+    protected $origin_y;
 
-	/**
-	 * Y coord of the anchor
-	 *
-	 * @var integer
-	 */
-	protected $anchor_y;
+    /**
+     * Constructor
+     * Sets the icon's image URL, width, height, origin and anchor
+     * default origin ( 0, 0 )
+     * default anchor ( w/2, h ).
+     *
+     * Throws an exception if the icon is invalid
+     *
+     * If allow_url_fopen is not enabled on your server you will need to explicity specify the dimensions of the icon
+     *
+     * @param string $icon    Absolute URL of the icon image
+     * @param array  $options
+     */
+    public function __construct($icon, array $options = null)
+    {
+        $this->icon = $icon;
 
-	/**
-	 * X coord of the origin
-	 *
-	 * @var integer
-	 */
-	protected $origin_x;
+        if (!isset($options['width']) || !isset($options['height'])) {
+            $size = @getimagesize($icon);
+            list($this->width, $this->height) = $size;
+        }
 
-	/**
-	 * Y coord of the origin
-	 *
-	 * @var integer
-	 */
-	protected $origin_y;
+        if (isset($options['width'])) {
+            $this->width = (int) $options['width'];
+        }
+        if (isset($options['height'])) {
+            $this->height = (int) $options['height'];
+        }
 
-	/**
-	 * Constructor
-	 * Sets the icon's image URL, width, height, origin and anchor
-	 * default origin ( 0, 0 )
-	 * default anchor ( w/2, h )
-	 *
-	 * Throws an exception if the icon is invalid
-	 *
-	 * If allow_url_fopen is not enabled on your server you will need to explicity specify the dimensions of the icon
-	 *
-	 * @param string $icon Absolute URL of the icon image
-	 * @throws Exception
-	 * @return MarkerIcon
-	 */
-	public function __construct( $icon, array $options = null ) {
+        $this->anchor_x = isset($options['anchor_x']) ? $options['anchor_x'] : floor($this->width / 2);
+        $this->anchor_y = isset($options['anchor_y']) ? $options['anchor_y'] : $this->height;
+        $this->origin_x = isset($options['origin_x']) ? $options['origin_x'] : 0;
+        $this->origin_y = isset($options['origin_y']) ? $options['origin_y'] : 0;
+    }
 
-		$this->icon = $icon;
+    /**
+     * Static create method useful for method chaining.
+     *
+     * @param string $icon URL of the icon image
+     *
+     * @return $this
+     */
+    public static function create($icon, array $options = null)
+    {
+        return new self($icon, $options);
+    }
 
-		if ( !isset( $options['width'] ) || !isset( $options['height'] ) ) {
-			$size = @getimagesize( $icon );
-			list( $this->width, $this->height ) = $size;
-		}
+    /**
+     * Set the icon's height.
+     *
+     * @param int $height
+     *
+     * @return $this
+     */
+    public function setHeight($height)
+    {
+        $this->height = (int) $height;
 
-		if ( isset( $options['width'] ) ) {
-			$this->width = (int) $options['width'];
-		}
-		if ( isset( $options['height'] ) ) {
-			$this->height = (int) $options['height'];
-		}
+        return $this;
+    }
 
-		$this->anchor_x = isset( $options['anchor_x'] ) ? $options['anchor_x'] : floor( $this->width / 2 );
-		$this->anchor_y = isset( $options['anchor_y'] ) ? $options['anchor_y'] : $this->height;
-		$this->origin_x = isset( $options['origin_x'] ) ? $options['origin_x'] : 0;
-		$this->origin_y = isset( $options['origin_y'] ) ? $options['origin_y'] : 0;
+    /**
+     * Set the icon's width.
+     *
+     * @param int $width
+     *
+     * @return $this
+     */
+    public function setWidth($width)
+    {
+        $this->width = (int) $width;
 
-		return $this;
+        return $this;
+    }
 
-	}
+    /**
+     * Set the icon's size (width/height).
+     *
+     * @param int $width
+     * @param int $height
+     *
+     * @return $this
+     */
+    public function setSize($width, $height)
+    {
+        $this->height = (int) $height;
+        $this->width = (int) $width;
 
-	/**
-	 * Static create method useful for method chaining
-	 *
-	 * @param string $icon URL of the icon image
-	 * @throws Exception
-	 * @return MarkerIcon
-	 */
-	public static function create( $icon, array $options=null ) {
-		return new MarkerIcon( $icon, $options );
-	}
+        return $this;
+    }
 
-	/**
-	 * Set the icon's height
-	 *
-	 * @param int $height
-	 * @return MarkerIcon
-	 */
-	public function setHeight( $height ) {
-		$this->height = (int) $height;
-		return $this;
-	}
+    /**
+     * Set the icon's anchor point
+     * This is the point on the icon that will be placed on the map.
+     *
+     * @param int $x X coordinate of the anchor
+     * @param int $y Y coordinate of the anchor
+     *
+     * @return $this
+     */
+    public function setAnchor($x = null, $y = null)
+    {
+        if ($x !== null) {
+            $this->anchor_x = (int) $x;
+        }
+        if ($y !== null) {
+            $this->anchor_y = (int) $y;
+        }
 
-	/**
-	 * Set the icon's width
-	 *
-	 * @param int $width
-	 * @return MarkerIcon
-	 */
-	public function setWidth( $width ) {
-		$this->width = (int) $width;
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * Set the icon's size (width/height)
-	 *
-	 * @param int $width
-	 * @param int $height
-	 * @return MarkerIcon
-	 */
-	public function setSize( $width, $height ) {
-		$this->height = (int) $height;
-		$this->width = (int) $width;
-		return $this;
-	}
+    /**
+     * Set the icon's origin point
+     * This is used when you are using a sprite for the marker.
+     *
+     * @param int $x X coord of the origin
+     * @param int $y Y coord of the origin
+     *
+     * @return $this
+     */
+    public function setOrigin($x = null, $y = null)
+    {
+        if ($x !== null) {
+            $this->origin_x = (int) $x;
+        }
+        if ($y !== null) {
+            $this->origin_y = (int) $y;
+        }
 
-	/**
-	 * Set the icon's anchor point
-	 * This is the point on the icon that will be placed on the map
-	 *
-	 * @param int $x X coord of the anchor
-	 * @param int $y Y coord of the anchor
-	 * @return MarkerIcon
-	 */
-	public function setAnchor( $x=null, $y=null ) {
-		if ( $x !== null ) {
-			$this->anchor_x = (int) $x;
-		}
-		if ( $y !== null ) {
-			$this->anchor_y = (int) $y;
-		}
-		return $this;
-	}
+        return $this;
+    }
 
-
-	/**
-	 * Set the icon's origin point
-	 * This is used when you are using a sprite for the marker
-	 *
-	 * @param int $x X coord of the origin
-	 * @param int $y Y coord of the origin
-	 * @return MarkerIcon
-	 */
-	public function setOrigin( $x=null, $y=null ) {
-		if ( $x !== null ) {
-			$this->origin_x = (int) $x;
-		}
-		if ( $y !== null ) {
-			$this->origin_y = (int) $y;
-		}
-		return $this;
-	}
-
-	/**
-	 * Return string
-	 */
-	public function __toString() {
-		return $this->icon;
-	}
-
+    /**
+     * Return string.
+     */
+    public function __toString()
+    {
+        return $this->icon;
+    }
 }
